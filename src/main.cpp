cbe7958443e78af47f9efaa3e865c3477fcc13a0
Julian <julian@britesc.com>
2025-10-24 20:49:26 +0200

    Added bsp library

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,23 @@
-#include <Arduino.h>
-#include "lvgl/lvgl.h"
+/**
+ * @file main.cpp
+ * @author Julian (51fiftyone51fiftyone@gmail.com)
+ * @brief Test of PlatformIO with LVGL Library
+ * @version 0.1
+ * @date 20251024
+ * @copyright Copyright (c) 2025
+ * 
+ */
 
+
+
+
+
+#include <Arduino.h>
+#include "lvgl.h"
+#include "Adafruit_GFX.h"
+#include "bsp_cst328.h"
+#include "lcd_defines/lcd_defines.h"
+#include "i2c_defines/i2c_defines.h"
 
 
 // put function declarations here:
