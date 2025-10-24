ff73968d4b58153f8453d24fec1da0f22b7cfa60
Julian <julian@britesc.com>
2025-10-24 22:58:43 +0200

    Trying to sort out GFX Library

--- src/main.cpp
+++ src/main.cpp
@@ -8,16 +8,35 @@
  * 
  */
 
-
-
-
-
+/*
+ * Start of Arduino Includes
+ */
 #include <Arduino.h>
-#include "lvgl.h"
-#include "Adafruit_GFX.h"
-#include "bsp_cst328.h"
+/*
+ * End Arduino Includes
+ */
+
+/*
+ * Start Arduino_GFX settings
+ */
+#include "Arduino_GFX_library.h"
 #include "lcd_defines/lcd_defines.h"
 #include "i2c_defines/i2c_defines.h"
+#include "Arduino_RPiPicoSPI.h"// provide the definition of Arduino_RPiPicoSPI
+
+Arduino_DataBus *bus = new Arduino_RPiPicoSPI(LCD_DC_PIN, LCD_CS_PIN, LCD_SCK_PIN, LCD_MOSI_PIN, LCD_MISO_PIN, LCD_SPI_NUM);
+
+/* More display class: https://github.com/moononournation/Arduino_GFX/wiki/Display-Class */
+Arduino_GFX *gfx = new Arduino_ST7789(
+  bus, LCD_RST_PIN, LCD_Orientation /* rotation */, true /* IPS */);
+ /*
+ * End Arduino_GFX settings
+ */
+
+#include "lvgl.h"
+
+#include "bsp_cst328.h"
+
 
 
 // put function declarations here:
